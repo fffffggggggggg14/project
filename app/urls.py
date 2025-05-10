3cdf3cf78cca050aceb96c48a8fd00ebac1bb03f
Mo <MO@gamil.com>
2025-05-11 00:12:36 +0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,6 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('todos/<int:todo_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
+    path('api/todos/<int:todo_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
 ]
