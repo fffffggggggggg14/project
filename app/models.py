1546cb065a85ed76ec5c38aebc76b5a90e5d2a2b
Mo <MO@gamil.com>
2025-05-10 23:32:37 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
 
 class Todo(models.Model):
     title = models.CharField(max_length = 255)
-    task = models.ForeignKey(Task, on_delete = models.PROTECT)
+    task = models.ForeignKey(Task, on_delete = models.CASCADE)
     create_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
