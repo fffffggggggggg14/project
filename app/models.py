976142d74390b121eb39fc51006600fe9e9216f9
Mo <MO@gamil.com>
2025-05-11 00:06:22 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -1,15 +1,15 @@
 from django.db import models
     
 class Task(models.Model):
-    task = models.CharField(max_length = 255)
-    description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    task = models.CharField(max_length = 200)
+    description = models.TextField(max_length = 10000)
+    create_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now = True)
     def __str__(self):
         return self.task
 
 class Todo(models.Model):
-    title = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 200)
     task = models.ForeignKey(Task, on_delete = models.CASCADE)
     create_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
