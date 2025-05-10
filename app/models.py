0367a5108ee51c602a5a3fcf80dca1d9bdb08463
Mo <MO@gamil.com>
2025-05-11 01:49:06 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(max_length=10000)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=50,
-        default='new',
-        choices=[
+    choices = [
             ('new', 'new'),
             ('in_progress', 'in_progress'),
             ('done', 'done'),
         ]
-    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, default='new',)
 
     def __str__(self):
         return self.title
@@ -21,9 +18,9 @@ class Task(models.Model):
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='todos')
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
