641920ec8d0012fa869293ab7ef75a9860e5a8e9
Mo <MO@gamil.com>
2025-05-12 01:20:59 +0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -5,13 +5,13 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 
 urlpatterns = [
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/todos/', TodoListCreateView.as_view(), name='todo-list-create'),
-    path('api/todos/<int:pk>/', TodoRetrieveUpdateDestroyView.as_view(), name='todo-detail'),
+    path('api/todos', TodoListCreateView.as_view(), name='todo-list-create'),
+    path('api/todos/<int:pk>', TodoRetrieveUpdateDestroyView.as_view(), name='todo-detail'),
 
-    path('api/todos/<int:todo_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('api/tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
+    path('api/todos/<int:todo_id>/tasks', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
 ]
 
