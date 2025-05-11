1f0ff8f8048ddb419c07497a1b884bf3545cc986
Mo <MO@gamil.com>
2025-05-12 00:19:00 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from PIL import Image
+import os
 
 class Task(models.Model):
     choices = [
@@ -11,6 +13,19 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=50, default='new',)
+
+    image = models.ImageField(upload_to='todo_images/', blank=True, null=True)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            image_path = self.image.path
+            img = Image.open(image_path).convert('RGB')
+            webp_path = os.path.splitext(image_path)[0] + '.webp'
+            img.save(webp_path, 'webp')
+            if not self.image.name.endswith('.webp'):
+                os.remove(image_path)
+                self.image.name = os.path.splitext(self.image.name)[0] + '.webp'
+                self.save(update_fields=['image'])
 
     def __str__(self):
         return self.title
