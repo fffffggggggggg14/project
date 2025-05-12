bde4d3c9a2efe5df3bef1b5fbc825141d75672ad
Mo <MO@gamil.com>
2025-05-12 21:08:55 +0300

    commit

--- app/urls.py
+++ app/urls.py
@@ -22,13 +22,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import TodoListCreateView, TodoRetrieveUpdateDestroyView, TaskListCreateView, TaskRetrieveUpdateDestroyView, TaskListCreateView, ImageRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/todos/', TodoListCreateView.as_view(), name='todo-list-create'),
-    path('api/todos/<int:pk>/', TodoRetrieveUpdateDestroyView.as_view(), name='todo-detail'),
-    path('api/todos/<int:todo_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
-    path('api/tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
-    path('api/tasks/<int:task_id>/images/', TaskListCreateView.as_view(), name='task-images-list-create'),
-    path('api/images/<int:pk>/', ImageRetrieveUpdateDestroyView.as_view(), name='image-detail'),
+    path('api/todos', TodoListCreateView.as_view(), name='todo-list-create'),
+    path('api/todos/<int:pk>', TodoRetrieveUpdateDestroyView.as_view(), name='todo-detail'),
+    
+    path('api/todos/<int:todo_id>/tasks', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
+
+    path('api/tasks/<int:task_id>/images', TaskListCreateView.as_view(), name='task-images-list-create'),
+    path('api/images/<int:pk>', ImageRetrieveUpdateDestroyView.as_view(), name='image-detail'),
 ]
