bb3b237ad7caaa0f984d419a9ea21e1572dd494f
Mo <MO@gamil.com>
2025-05-13 01:00:06 +0300

    commit

--- admins_api/urls.py
+++ admins_api/urls.py
@@ -5,9 +5,12 @@ from .views import  CustomAdminListCreateView, CustomAdminRetrieveUpdateDestroyV
 urlpatterns = [
     path('api/admins/', CustomAdminListCreateView.as_view(), name='admin-list-create'),
     path('api/admins/<int:pk>/', CustomAdminRetrieveUpdateDestroyView.as_view(), name='admin-detail'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('api/register/', RegisterView.as_view(), name='user_register'),
+    
     path('api/password_reset/', PasswordResetRequestView.as_view(), name='password_reset_request'),
     path('api/password_reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
